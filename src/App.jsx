89d43765533017e8d5a464fd7adc513175f4d62f
Khizarazam <164862318+Khizarazam@users.noreply.github.com>
2024-08-13 19:00:58 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,19 +22,19 @@ function App() {
 
     const frames = {
       currentIndex: 0,
-      maxIndex: 382
+      maxIndex: 382,
     };
 
-    let imagesLoaded = 0;
     const images = [];
+    let imagesLoaded = 0;
 
     function preloadImages() {
       for (let i = 1; i <= frames.maxIndex; i++) {
-        const imageUrl =  `/scroll-animation/compressed_images/frame_${i.toString().padStart(4, "0")}.jpg`;
+        const imageUrl = `/scroll-animation/compressed_images/frame_${i.toString().padStart(4, "0")}.jpg`;
         const img = new Image();
         img.src = imageUrl;
         img.onload = () => {
-          imagesLoaded++
+          imagesLoaded++;
           if (imagesLoaded === frames.maxIndex) {
             LoadImage(frames.currentIndex);
             startAnimation();
@@ -47,59 +47,48 @@ function App() {
     function LoadImage(index) {
       if (index >= 0 && index <= frames.maxIndex) {
         const img = images[index];
-
         const scaleX = canvas.width / img.width;
         const scaleY = canvas.height / img.height;
         const scale = Math.max(scaleX, scaleY);
-
         const newWidth = img.width * scale;
         const newHeight = img.height * scale;
-
         const offsetX = (canvas.width - newWidth) / 2;
         const offsetY = (canvas.height - newHeight) / 2;
-
         context.clearRect(0, 0, canvas.width, canvas.height);
         context.imageSmoothingEnabled = true;
-        context.imageSmoothingQuality = "high";
+        context.imageSmoothingQuality = 'high';
         context.drawImage(img, offsetX, offsetY, newWidth, newHeight);
         frames.currentIndex = index;
       }
     }
 
     function startAnimation() {
-
-      var tl = gsap.timeline({
+      gsap.timeline({
         scrollTrigger: {
-          trigger: ".parent",
-          start: "top top",
+          trigger: '.parent',
+          start: 'top top',
           scrub: 2,
-          end: "bottom bottom",
-
-        }
-      });
-
-      tl.to(frames, {
+          end: 'bottom bottom',
+        },
+      }).to(frames, {
         currentIndex: frames.maxIndex,
         onUpdate: function () {
           LoadImage(Math.floor(frames.currentIndex));
-        }
+        },
       });
     }
 
-    preloadImages()
-  }, [])
+    preloadImages();
+  }, []);
 
   return (
-    <>
-      <div className="w-full bg-zinc-900">
-
-        <div className="parent relative top-0 left-0 w-full h-[1400vh]">
-          <div className="w-full sticky top-0 left-0 h-screen">
-            <canvas ref={canvasRef} id="frame" className="w-full h-screen"></canvas>
-          </div>
+    <div className="w-full bg-zinc-900">
+      <div className="parent relative top-0 left-0 w-full h-[1400vh]">
+        <div className="w-full sticky top-0 left-0 h-screen">
+          <canvas ref={canvasRef} id="frame" className="w-full h-screen"></canvas>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
